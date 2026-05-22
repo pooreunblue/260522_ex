1f9bbd6ffc08c450fcf52d0e60a7e7aa1540add5
bsh <mjbsh02@mju.ac.kr>
2026-05-22 09:30:28 +0900

    feat: for, while 반복문 및 배열 순회 실습 추가

--- 01_for.js
+++ 01_for.js
@@ -1,1 +1,28 @@
-// while
+// if, switch - 1번 => 여러번?
+// 특정한 조건을 만족시키면 1번
+// while - 특정한 조건이 만족된다면 계속 진행
+// -> 조건/논리값 - boolean -> 뒤집으면 false
+
+// while (true) { // 무한반복 }
+    let w = 0;
+    console.log(w);
+    w++;
+
+// for (초기식;조건식;증감식) {}
+// for (const i = 0; i < 5; i++) { // Assignment to constant variable.}
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+// for-in, for-of -> const v => 한 블록마다 호출되고 그 다음에 소멸함.
+
+//  배열 순회
+const arr = ["아침","간식","점심","간식","저녁"]
+for (let i=0; i < arr.length; i++) {
+    if (arr[i] === "간식") {
+        continue;
+    }
+    console.log(arr[i]);
+    if (arr[i] === "점심") {
+        break;
+    }
+}
